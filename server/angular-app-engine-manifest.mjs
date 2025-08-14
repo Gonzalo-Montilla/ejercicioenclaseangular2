1c70deb3220e0693ea8a1ad233822eadb40f5f25
gonzalo <you@example.com>
2025-08-13 20:40:34 -0500

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://Gonzalo-Montilla.github.io/ejercicioenclaseangular2',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
